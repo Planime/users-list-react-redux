511f4fa37aab9f76f42f1097e6a8f18331636121
digtiar <eddydigtiar@gmail.com>
2021-04-12 22:06:09 -0700

    created new project

---
+++ src/apiServices/apiUsers.js
@@ -0,0 +1,11 @@
+import axios from "axios";
+
+
+
+
+export default axios.create({
+        baseURL: "https://5fec128e573752001730b0f1.mockapi.io/users",
+        headers: {
+            "Content-Type": "application/json",
+        }
+    });
